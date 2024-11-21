5831109bde3a8f72cc0f20720ae6b14f2c125cb0
Rafael Costa Oliveira <123006410+rafwcosta@users.noreply.github.com>
2024-11-21 10:56:53 -0300

    Mudança de nome Exemplo 1

--- src/Exp1.java
+++ src/Exp1.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Locale;
 
-public class Exp1 {
+public class Exemplo1 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
